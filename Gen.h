e3a87741c62a250dff99eee2d6c982dbfb59e1d7
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-03-03 15:52:29 +0200

    Small config updates

--- Gen.h
+++ Gen.h
@@ -23,21 +23,21 @@ struct Gen
 const std::map<Trigger, unsigned int> CREATION_COST = {
 	{Trigger::Stay, 1},
 	{Trigger::Move, 15},
-	{Trigger::Turn, 10},
-	{Trigger::Photosyntesis, 15},
+	{Trigger::Turn, 2},
+	{Trigger::Photosyntesis, 10},
 	{Trigger::Mineraleon, 15},
 	{Trigger::Carnivorous, 25},
 	{Trigger::Birth, 30},
-	{Trigger::Separate, 40}
+	{Trigger::Separate, 30}
 };
 
 const std::map<Trigger, unsigned int> MAINTENANACE_COST = {
 	{Trigger::Stay, 1},
 	{Trigger::Move, 5},
-	{Trigger::Turn, 2},
-	{Trigger::Photosyntesis, 5},
-	{Trigger::Mineraleon, 5},
-	{Trigger::Carnivorous, 10},
-	{Trigger::Birth, 12},
-	{Trigger::Separate, 15}
+	{Trigger::Turn, 1},
+	{Trigger::Photosyntesis, 1},
+	{Trigger::Mineraleon, 2},
+	{Trigger::Carnivorous, 5},
+	{Trigger::Birth, 6},
+	{Trigger::Separate, 6}
 };

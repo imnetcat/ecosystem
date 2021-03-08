d1cee4993a93065b26fd44e1ca493aeb597354e0
Radioactive lab <37350375+imnetcat@users.noreply.github.com>
2021-03-09 00:21:44 +0200

    Update config.h

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ const short OUTLINE = 1;
 const short CELL_SIZE = 10;
 const short CELL_OUTLINE = CELL_SIZE + OUTLINE;
 
-const unsigned short LIGHT_POWER = 250;
+const unsigned short LIGHT_POWER = 550;
 const double LIGHT_COEF = 0.6;
 
 const unsigned short MAX_MINERALS_TO_EAT = 500;
